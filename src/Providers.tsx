c58c0c1808643240b4306d7920cf9fbab966e9d7
charilscah <chrlschwb77@gmail.com>
2023-02-03 05:36:26 -0800

    test

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -2,7 +2,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_QN_URL,
+  // uri: import.meta.env.VITE_QN_URL,
+  uri: "https://query.joystream.org/graphql",
   cache: new InMemoryCache(),
   connectToDevTools: true,
   defaultOptions: {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 import { useElectedCouncils } from '@/hooks';
 
+import { Col, Row, Table } from 'react-bootstrap';
+import "./home.css";
+
+
 export default function Home() {
   const { data } = useElectedCouncils({});
   console.log(data);
-  return <div>Home</div>;
+  return <div className='main'>
+    <div className='justify-content-center'>
+      <span style={{ fontSize: "30px" }}>
+        COUNCIL PERIOD : &nbsp;
+      </span>
+      <select name="cars" className="select_input">
+        {(data === undefined) ? "" : data.electedCouncils.map((data, i) =>
+          <option value={data.id} key={i}>{data.id}</option>)
+        }
+      </select>
+      <hr />
+    </div>
+
+  </div>;
 }
