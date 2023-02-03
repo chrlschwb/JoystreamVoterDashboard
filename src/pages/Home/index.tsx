4b3cff80908d81b28d710843e1ddd77ca3a0841e
charilscah <chrlschwb77@gmail.com>
2023-02-03 06:13:39 -0800

    mytest

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useElectedCouncils } from '@/hooks';
 
@@ -8,19 +8,20 @@ import "./home.css";
 
 export default function Home() {
   const { data } = useElectedCouncils({});
-  console.log(data);
+
+  const [period, setPeriod] = useState<number>(0);
+
   return <div className='main'>
     <div className='justify-content-center'>
       <span style={{ fontSize: "30px" }}>
         COUNCIL PERIOD : &nbsp;
       </span>
-      <select name="cars" className="select_input">
+      <select name="cars" className="select_input" onChange={(e) => setPeriod(Number(e.target.value))}>
         {(data === undefined) ? "" : data.electedCouncils.map((data, i) =>
-          <option value={data.id} key={i}>{data.id}</option>)
+          <option key={i}>{data.id}</option>)
         }
       </select>
       <hr />
     </div>
-
   </div>;
 }
